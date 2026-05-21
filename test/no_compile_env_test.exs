8c0ff5cecc2396f2558ac4a4cdc76b40df745e5f
Matthew Gilliam <chase.gilliam@gmail.com>
2026-05-21 12:08:31 -0400

    test: add #122 compile_env guard and percentage distribution tests

---
+++ test/no_compile_env_test.exs
@@ -0,0 +1,19 @@
+defmodule NoCompileEnvTest do
+  @moduledoc """
+  Bandera's whole reason for existing is to avoid compile-time configuration
+  (fun_with_flags#122). This test fails if an `Application.compile_env` call is
+  ever introduced into the library source. (A doc/comment mention using the
+  `Application.compile_env/3` arity form is allowed and does not match.)
+  """
+  use ExUnit.Case, async: true
+
+  test "lib/ contains no Application.compile_env call" do
+    offenders =
+      "lib/**/*.ex"
+      |> Path.wildcard()
+      |> Enum.filter(fn path -> File.read!(path) =~ ~r/Application\.compile_env[\s(]/ end)
+
+    assert offenders == [],
+           "Application.compile_env must not be used in Bandera. Offending files: #{inspect(offenders)}"
+  end
+end
